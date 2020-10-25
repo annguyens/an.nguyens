a35f7117f18a3dd3cae767d8cd29171ad60cfe69
annguyens <an.nguyen8@mavs.uta.edu>
2020-10-25 01:41:48 -0500

    activities page structure- added SASE slideshow

--- vendor/bootstrap/js/bootstrap.js
+++ vendor/bootstrap/js/bootstrap.js
@@ -57,6 +57,26 @@
     return keys;
   }
 
+  var slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2500); // Change image every 2.5 seconds
+  }
+
   function _objectSpread2(target) {
     for (var i = 1; i < arguments.length; i++) {
       var source = arguments[i] != null ? arguments[i] : {};
